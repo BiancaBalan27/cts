b66c25b829d218c63f5c7e31a4741289964b4ce7
BiancaBalan27 <biancabalan333@gmail.com>
2021-02-25 13:31:21 +0200

    commit

--- Seminar1/src/pachet/Main.java
+++ Seminar1/src/pachet/Main.java
@@ -1,6 +1,7 @@
 package pachet;
 
 import clase.Giraffe;
+import clase.Zebra;
 import clase.Zoo;
 
 public class Main {
@@ -10,8 +11,12 @@ public class Main {
 		Zoo zoo = new Zoo();
 		Giraffe g1 = new Giraffe("Maia", 300);
 		Giraffe g2 = new Giraffe("Lola");
+		Zebra z1 = new Zebra("Zebra1", 350);
+		Zebra z2 = new Zebra("Zebra2");
 		zoo.addAnimal(g1);
 		zoo.addAnimal(g2);
+		zoo.addAnimal(z1);
+		zoo.addAnimal(z2);
 		zoo.feedAllAnimals();
 	}
 
