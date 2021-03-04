4d5521304fe7358db6303b0229c5474a5740b5dd
BiancaBalan27 <biancabalan333@gmail.com>
2021-03-04 12:22:17 +0200

    another commit

---
+++ Seminar2/src/ro/ase/cts/clase/UniversalReader.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.clase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UniversalReader {
+	//punct de legatura catre readere
+}
