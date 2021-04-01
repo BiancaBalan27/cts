dbe837f6ae80d032fe47bce06cec601a2eafe2ea
BiancaBalan27 <biancabalan333@gmail.com>
2021-04-01 13:18:00 +0300

    adapter

---
+++ Seminar6_builder/src/ro/ase/cts/builder/clase/RezervareBuilderV2.java
@@ -0,0 +1,57 @@
+package ro.ase.cts.builder.clase;
+
+public class RezervareBuilderV2 implements Builder {
+	
+	private int codRezervare;
+	private boolean areMancare;
+	private boolean areScaunErgonomic;
+	private boolean areBautura;
+	private boolean areMuzica;
+	private String genMuzica;
+	
+	public RezervareBuilderV2() {
+		super();
+		this.codRezervare = 0;
+		this.areMancare = false;
+		this.areScaunErgonomic = false;
+		this.areBautura = false;
+		this.areMuzica = false;
+		this.genMuzica = " ";
+	}
+	
+	public RezervareBuilderV2 setCodRezervare(int codRezervare) {
+		this.codRezervare = codRezervare;
+		return this;
+	}
+
+	public RezervareBuilderV2 setAreMancare(boolean areMancare) {
+		this.areMancare = areMancare;
+		return this;
+	}
+
+	public RezervareBuilderV2 setAreScaunErgonomic(boolean areScaunErgonomic) {
+		this.areScaunErgonomic = areScaunErgonomic;
+		return this;
+	}
+
+	public RezervareBuilderV2 setAreBautura(boolean areBautura) {
+		this.areBautura = areBautura;
+		return this;
+	}
+
+	public RezervareBuilderV2 setAreMuzica(boolean areMuzica) {
+		this.areMuzica = areMuzica;
+		return this;
+	}
+
+	public RezervareBuilderV2 setGenMuzica(String genMuzica) {
+		this.genMuzica = genMuzica;
+		return this;
+	}
+
+	@Override
+	public Rezervare build() {
+		return new Rezervare(codRezervare, areMancare, areScaunErgonomic, areBautura,
+				areMuzica, genMuzica);
+	}
+}
--- Seminar6_builder/src/ro/ase/cts/builder/program/Main.java
+++ Seminar6_builder/src/ro/ase/cts/builder/program/Main.java
@@ -2,6 +2,7 @@ package ro.ase.cts.builder.program;
 
 import ro.ase.cts.builder.clase.Rezervare;
 import ro.ase.cts.builder.clase.RezervareBuilder;
+import ro.ase.cts.builder.clase.RezervareBuilderV2;
 
 public class Main {
 
@@ -11,6 +12,12 @@ public class Main {
 		Rezervare rezervare2 = new RezervareBuilder().setAreMancare(true).setAreBautura(true).build();
 		System.out.println(rezervare1);
 		System.out.println(rezervare2);
+		
+		RezervareBuilderV2 rezervareBuilder2 = new RezervareBuilderV2();
+		Rezervare rezervare3 = rezervareBuilder2.setAreBautura(true).build();
+		Rezervare rezervare4 = rezervareBuilder2.setAreMancare(true).setAreBautura(true).build();
+		System.out.println(rezervare3);
+		System.out.println(rezervare4);
 
 	}
 
